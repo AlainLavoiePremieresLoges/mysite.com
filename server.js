e24fa09842124e0a5422d517365adc845ec1f8b2
alainlavoie <alain@premieresloges.ca>
2015-08-28 14:58:01 -0400

    ok

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ var http = require('http');
 
 var myServer="ServerA"
 var myCounter=0;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(myServer + ': Hello World' + myCounter '\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var serverPort = 1337;
+var serverAddress = '192.168.1.164';
+
+process.argv.forEach(function (val, index, array) {
+    //console.log(index + ': ' + val);
+    switch( index ){
+       case 2:
+   	 myServer=val;
+	 break;
+       case 3:
+   	 serverPort=val;
+	 break;
+       case 4:
+   	 serverAddress=val;
+	 break;
+    }
+});
+
+var singleFunction = function (req, res) {
+    if ( req.url === '/favicon.ico' ){
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end();
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(myServer + ': Hello World ' + myCounter + '\n');
+        myCounter = myCounter +1 ;
+    }
+};
+http.createServer(singleFunction).listen(serverPort, serverAddress);
+console.log('Server running at http://' + serverAddress  + ':' + serverPort + '/');
