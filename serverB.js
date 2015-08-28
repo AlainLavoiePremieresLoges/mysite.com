b6749a7d0e2ffaf0f8726547fa1d283475019ecf
alainlavoie <alain@premieresloges.ca>
2015-08-28 13:25:49 -0400

    ok

--- serverB.js
+++ serverB.js
@@ -4,6 +4,6 @@ var myServer="ServerB"
 var myCounter=0;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(myServer + ': Hello World' + myCounter '\n');
+    res.end(myServer + ': Hello World' + myCounter + '\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
