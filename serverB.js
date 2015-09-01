3cb8a92f4d8271a25651d9dbcc4e664a98c337e9
alainlavoie <alain@premieresloges.ca>
2015-09-01 14:55:41 -0400

    for server upgrade

--- serverB.js
+++ serverB.js
@@ -14,6 +14,6 @@ var server = app.listen(serverPort, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s, counter=%d', host, port, myCounter++);
 });
 
