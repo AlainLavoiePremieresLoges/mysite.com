c2da0af057bb2a5569767885c58df170c96f05dc
alainlavoie <alain@premieresloges.ca>
2015-09-01 15:35:47 -0400

    for server upgrade

--- serverB.js
+++ serverB.js
@@ -3,24 +3,30 @@ var myCounter=0;
 var serverPort = 3001;
 var serverAddress = '192.168.1.142';
 
-var FileStreamRotator = require('file-stream-rotator')
+//var FileStreamRotator = require('file-stream-rotator')
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var fs = require('fs');
 var logDirectory = __dirname + '/logs';
+var winston = require('winston');
+ 
+winston.log('info', 'Hello distributed log files!');
+winston.info('Hello again distributed logs');
+winston.level = 'debug';
+winston.log('debug', 'Now my debug messages are written to console!');
 
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+//fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 // create a rotating write stream
-var accessLogStream = FileStreamRotator.getStream({
-  filename: logDirectory + '/access-%DATE%.log',
-  frequency: 'daily',
-  verbose: false
-})
+//var accessLogStream = FileStreamRotator.getStream({
+//  filename: logDirectory + '/access-%DATE%.log',
+//  frequency: 'daily',
+//  verbose: false
+//})
 
 // setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
+//app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', function (req, res) {
   res.send('Express site, counter=' + myCounter++);
