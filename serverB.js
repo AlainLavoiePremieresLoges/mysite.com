ca9732ee2cd992b498753063fbf08d726a306d86
alainlavoie <alain@premieresloges.ca>
2015-09-01 15:51:49 -0400

    for server upgrade

--- serverB.js
+++ serverB.js
@@ -24,9 +24,9 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen(serverPort, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    logger.log('debug', 'Example app listening at http://%s:%s', host, port);
 });
 
