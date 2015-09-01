ac2d1c112d0957eff1ea2a0bcf640e32dc55ae50
alainlavoie <alain@premieresloges.ca>
2015-09-01 14:43:42 -0400

    for server upgrade

--- serverB.js
+++ serverB.js
@@ -1,34 +1,19 @@
-var http = require('http');
-
-var myServer="ServerA"
+var myServer="ServerB"
 var myCounter=0;
-var serverPort = 1337;
-var serverAddress = '192.168.1.164';
+var serverPort = 5001;
+var serverAddress = '192.168.1.142';
 
-process.argv.forEach(function (val, index, array) {
-    //console.log(index + ': ' + val);
-    switch( index ){
-       case 2:
-   	 myServer=val;
-	 break;
-       case 3:
-   	 serverPort=val;
-	 break;
-       case 4:
-   	 serverAddress=val;
-	 break;
-    }
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-var singleFunction = function (req, res) {
-    if ( req.url === '/favicon.ico' ){
-        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-        res.end();
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(myServer + ': Hello World ' + myCounter + '\n');
-        myCounter = myCounter +1 ;
-    }
-};
-http.createServer(singleFunction).listen(serverPort, serverAddress);
-console.log('Server running at http://' + serverAddress  + ':' + serverPort + '/');
+var server = app.listen(serverPort, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
