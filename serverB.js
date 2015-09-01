7e3bb0e03c7c84f998278d91849dc92db1722914
alainlavoie <alain@premieresloges.ca>
2015-09-01 15:30:56 -0400

    for server upgrade

--- serverB.js
+++ serverB.js
@@ -3,8 +3,24 @@ var myCounter=0;
 var serverPort = 3001;
 var serverAddress = '192.168.1.142';
 
+var FileStreamRotator = require('file-stream-rotator')
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+var fs = require('fs');
+var logDirectory = __dirname + '/logs';
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.get('/', function (req, res) {
   res.send('Express site, counter=' + myCounter++);
